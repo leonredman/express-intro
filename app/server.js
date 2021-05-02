d44f057f35ab21964ff4d681895803a2472fa659
leonredman <leonproart@yahoo.com>
2021-05-02 00:37:00 -0700

    add database and body parser

--- app/server.js
+++ app/server.js
@@ -1,10 +1,16 @@
 //set up basic express server
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const DB = require("../database/connection");
+const bodyParser = require("body-parser");
+const port = 3000;
 
 // Routes
 const routes = require("./routes.js");
+
+// Using Body parser for post request data
+app.use(bodyParser.urlencoded({extended: true}));
+
 // Static Files Folder
 app.use(express.static("public"));
 // Template Engine
